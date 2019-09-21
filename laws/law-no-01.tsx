44292a08143955502d67dae36a4f479bfd379a93
Alex Devero <deveroalex@gmail.com>
2019-09-21 14:49:08 +0200

    Add content for law number 1

--- laws/law-no-01.tsx
+++ laws/law-no-01.tsx
@@ -10,28 +10,26 @@ import Styles from './../constants/styles'
 
 export default function LawOneScreen(props) {
   return (
-    <ScrollView>
+    <ScrollView style={Styles.viewContainerStyle}>
       <View>
-        <Text>Laws no.1:</Text>
+        <Text style={Styles.lawHeadingStyles}>Laws no.1:</Text>
 
-        <Text>Aesthetic Usability Effect</Text>
+        <Text style={Styles.lawHeadingStyles}>Aesthetic Usability Effect</Text>
 
-        <Text>Summary:</Text>
+        <Text style={Styles.lawSubheadingStyles}>Summary:</Text>
 
-        <Text>Users often perceive aesthetically pleasing design as design that’s more usable.</Text>
+        <Text style={Styles.lawTextStyles}>Users often perceive aesthetically pleasing design as design that’s more usable.</Text>
 
-        <View>
-          <Text>Key Takeaways:</Text>
-          <Text>- Aesthetically pleasing design can make users more tolerant of minor usability issues.</Text>
-          <Text>- Aesthetically pleasing design can mask usability problems and prevent issues from being discovered during usability testing.</Text>
-        </View>
+        <Text style={Styles.lawSubheadingStyles}>Key Takeaways:</Text>
+        <Text style={Styles.lawTextStyles}>- Aesthetically pleasing design can make users more tolerant of minor usability issues.</Text>
+        <Text style={Styles.lawTextStyles}>- Aesthetically pleasing design can mask usability problems and prevent issues from being discovered during usability testing.</Text>
 
-        <Text>Origins:</Text>
+        <Text style={Styles.lawSubheadingStyles}>Origins:</Text>
 
-        <Text>The aesthetic-usability effect was first studied in the field of human–computer interaction in 1995. Researchers Masaaki Kurosu and Kaori Kashimura from the Hitachi Design Center tested 26 variations of an ATM UI, asking the 252 study participants to rate each design on ease of use, as well as aesthetic appeal. They found a stronger correlation between the participants’ ratings of aesthetic appeal and perceived ease of use than the correlation between their ratings of aesthetic appeal and actual ease of use. Kurosu and Kashimura concluded that users are strongly influenced by the aesthetics of any given interface, even when they try to evaluate the underlying functionality of the system.</Text>
+        <Text style={Styles.lawTextStyles}>The aesthetic-usability effect was first studied in the field of human–computer interaction in 1995. Researchers Masaaki Kurosu and Kaori Kashimura from the Hitachi Design Center tested 26 variations of an ATM UI, asking the 252 study participants to rate each design on ease of use, as well as aesthetic appeal. They found a stronger correlation between the participants’ ratings of aesthetic appeal and perceived ease of use than the correlation between their ratings of aesthetic appeal and actual ease of use. Kurosu and Kashimura concluded that users are strongly influenced by the aesthetics of any given interface, even when they try to evaluate the underlying functionality of the system.</Text>
 
         <View>
-          <Text onPress={() => props.navigation.navigate('List')}>Start learning</Text>
+          <Text style={Styles.lawBackLinkStyles} onPress={() => props.navigation.navigate('List')}>Go back to list</Text>
         </View>
       </View>
     </ScrollView>
