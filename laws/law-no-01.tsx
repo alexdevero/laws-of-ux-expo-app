def633cdee5eb25d8db15eb0e677db437435e3fc
Alex Devero <deveroalex@gmail.com>
2019-09-22 05:27:56 +0200

    Update lawStyles for styling in law one screen

--- laws/law-no-01.tsx
+++ laws/law-no-01.tsx
@@ -10,26 +10,26 @@ import Styles from './../constants/styles'
 
 export default function LawOneScreen(props) {
   return (
-    <ScrollView style={Styles.viewContainerStyle}>
+    <ScrollView style={[Styles.viewContainerStyle, Styles.lawStyles.lawContainerStyles]}>
       <View>
-        <Text style={Styles.lawHeadingStyles}>Laws no.1:</Text>
+        <Text style={Styles.lawStyles.lawTertiaryHeadingStyles}>Law no.1:</Text>
 
-        <Text style={Styles.lawHeadingStyles}>Aesthetic Usability Effect</Text>
+        <Text style={Styles.lawStyles.lawPrimaryHeadingStyles}>Aesthetic Usability Effect</Text>
 
-        <Text style={Styles.lawSubheadingStyles}>Summary:</Text>
+        <Text style={Styles.lawStyles.lawSecondaryHeadingStyles}>Summary:</Text>
 
-        <Text style={Styles.lawTextStyles}>Users often perceive aesthetically pleasing design as design that’s more usable.</Text>
+        <Text style={Styles.lawStyles.lawTextStyles}>Users often perceive aesthetically pleasing design as design that’s more usable.</Text>
 
-        <Text style={Styles.lawSubheadingStyles}>Key Takeaways:</Text>
-        <Text style={Styles.lawTextStyles}>- Aesthetically pleasing design can make users more tolerant of minor usability issues.</Text>
-        <Text style={Styles.lawTextStyles}>- Aesthetically pleasing design can mask usability problems and prevent issues from being discovered during usability testing.</Text>
+        <Text style={Styles.lawStyles.lawSecondaryHeadingStyles}>Key Takeaways:</Text>
+        <Text style={Styles.lawStyles.lawTextStyles}>- Aesthetically pleasing design can make users more tolerant of minor usability issues.</Text>
+        <Text style={Styles.lawStyles.lawTextStyles}>- Aesthetically pleasing design can mask usability problems and prevent issues from being discovered during usability testing.</Text>
 
-        <Text style={Styles.lawSubheadingStyles}>Origins:</Text>
+        <Text style={Styles.lawStyles.lawSecondaryHeadingStyles}>Origins:</Text>
 
-        <Text style={Styles.lawTextStyles}>The aesthetic-usability effect was first studied in the field of human–computer interaction in 1995. Researchers Masaaki Kurosu and Kaori Kashimura from the Hitachi Design Center tested 26 variations of an ATM UI, asking the 252 study participants to rate each design on ease of use, as well as aesthetic appeal. They found a stronger correlation between the participants’ ratings of aesthetic appeal and perceived ease of use than the correlation between their ratings of aesthetic appeal and actual ease of use. Kurosu and Kashimura concluded that users are strongly influenced by the aesthetics of any given interface, even when they try to evaluate the underlying functionality of the system.</Text>
+        <Text style={Styles.lawStyles.lawTextStyles}>The aesthetic-usability effect was first studied in the field of human–computer interaction in 1995. Researchers Masaaki Kurosu and Kaori Kashimura from the Hitachi Design Center tested 26 variations of an ATM UI, asking the 252 study participants to rate each design on ease of use, as well as aesthetic appeal. They found a stronger correlation between the participants’ ratings of aesthetic appeal and perceived ease of use than the correlation between their ratings of aesthetic appeal and actual ease of use. Kurosu and Kashimura concluded that users are strongly influenced by the aesthetics of any given interface, even when they try to evaluate the underlying functionality of the system.</Text>
 
         <View>
-          <Text style={Styles.lawBackLinkStyles} onPress={() => props.navigation.navigate('List')}>Go back to list</Text>
+          <Text style={Styles.lawStyles.lawBackLinkStyles} onPress={() => props.navigation.navigate('List')}>&larr; Go back to list</Text>
         </View>
       </View>
     </ScrollView>
