be83b65d0ab8ea54627b32a180f1b52861602329
Alex Devero <deveroalex@gmail.com>
2019-10-05 11:39:07 +0200

    Fix content issues in law 2

--- laws/law-no-02.tsx
+++ laws/law-no-02.tsx
@@ -20,14 +20,14 @@ export default function LawOneScreen(props) {
 
         <Text style={styles.lawSecondaryHeadingStyles}>Summary:</Text>
 
-        <Text style={styles.lawTextStyles}>Productivity soars when a computer and its users interact at a pace (<400ms) that ensures that neither has to wait on the other.</Text>
+        <Text style={styles.lawTextStyles}>Productivity soars when a computer and its users interact at a pace (&lt;400ms) that ensures that neither has to wait on the other.</Text>
 
         <Text style={styles.lawSecondaryHeadingStyles}>Key Takeaways:</Text>
         <Text style={styles.lawTextStyles}>- Provide system feedback within 400ms in order to keep users’ attention and increase productivity.</Text>
 
         <Text style={styles.lawSecondaryHeadingStyles}>Origins:</Text>
 
-        <Text style={styles.lawTextStyles}>In 1982 Walter J. Doherty and Ahrvind J. Thadani published, in the IBM Systems Journal, a research paper that set the requirement for computer response time to be 400 milliseconds, not 2,000 (2 seconds) which had been the previous standard. When a human being’s command was executed and returned an answer In under 400 milliseconds, it was deemed to exceed the Doherty threshold, and use of such applications were deemed to be “addicting” to users.</Text>
+        <Text style={styles.lawTextStyles}>In 1982 Walter J. Doherty and Ahrvind J. Thadani published, in the IBM Systems Journal, a research paper that set the requirement for computer response time to be 400 milliseconds, not 2,000 (2 seconds) which had been the previous standard. When a human being’s command was executed and returned an answer In under 400 milliseconds, it was deemed to exceed the Doherty threshold, and use of such applications were deemed to be "addicting" to users.</Text>
 
         <View style={styles.lawBackLinkContainerStyles}>
           <Text accessibilityRole="button" style={styles.lawBackLinkStyles} onPress={() => props.navigation.navigate('List')}>&larr; Go back to list</Text>
